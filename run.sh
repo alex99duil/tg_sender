cf89008d77c9e0a2f534676e6eee86ec01836f58
alex <alex99duil@gmail.com>
2023-07-21 20:29:49 +0000

    random fixes

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 function install(){
+    python3 -m pip install --user --upgrade pip
+    python3 -m pip install --user virtualenv
     python3 -m venv env
     source ./env/bin/activate
     python3 -m pip install --upgrade pip
     python3 -m pip install -r requirements.txt
-    python3 main.py join
+    python3 main.py join_to_chats
 }
 
 function run() {
